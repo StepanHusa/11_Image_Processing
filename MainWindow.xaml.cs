87381add8d314a5bbde1ed68b4e8331ab8e43bc2
StepanHusa <stepan.husa@gmail.com>
2021-07-15 15:06:22 +0200

    Add project files.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using PdfSharp;
+using PdfSharp.Pdf;
+using PdfSharp.Drawing;
+using System.Diagnostics;
+using System.Drawing;
+
+namespace _11_Image_Processing
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        string tempPdf;
+        string tempSetting;
+
+        public MainWindow()
+        {
+            System.Text.Encoding.RegisterProvider(System.Text.CodePagesEncodingProvider.Instance);
+            InitializeComponent();
+            tempPdf = System.IO.Path.GetTempFileName();
+            HelloWorld();
+        }
+
+        private void ButtonOpenClearPdf_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            if (openFileDialog.ShowDialog() != true) return;
+            var f=openFileDialog.FileName;
+
+        }
+
+        private void Edit_Click(object sender, RoutedEventArgs e)
+        {
+            this.DataContext = new ViewModel();
+            PDFViewer.Visibility = Visibility.Visible;
+        }
+
+        private void HelloWorld()
+        {
+            PdfDocument doc = new();
+            PdfPage page = doc.AddPage();
+            XGraphics gfx = XGraphics.FromPdfPage(page);
+            XFont font = new("Arial", 20);
+            gfx.DrawString("Hello, World!", font, XBrushes.Black,new XRect(0, 0, page.Width, page.Height),XStringFormats.Center);
+            string filename = "HelloWorld.pdf";
+            doc.Save(filename);
+            //Process.Start(filename);
+
+        }
+    }
+}
---
+++ ViewModel.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.ComponentModel;
+
+namespace _11_Image_Processing
+{
+    public class ViewModel : INotifyPropertyChanged
+    {
+        string path = @"D:\0_DW\marketa.duskova_2021-06-04_08-50-23.pdf";
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged(PropertyChangedEventArgs e)
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, e);
+        }
+
+        private Stream docStream;
+        public Stream DocumentStream
+        {
+            get { return docStream; }
+            set { docStream = value; OnPropertyChanged(new PropertyChangedEventArgs("DocumentStream")); }
+        }
+
+
+        public ViewModel()
+        {
+
+            docStream = new FileStream(path, FileMode.OpenOrCreate);
+        }
+
+    }
+}
